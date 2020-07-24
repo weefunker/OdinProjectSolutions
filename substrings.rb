c684b35e86bc501297b03f7d44c32d166c942548
Alex Overend <weefunker@yahoo.co.uk>
2020-07-24 17:18:51 +0100

    Update substrings.rb
    Getting close, just need to figure out how to pass this as a block

--- substrings.rb
+++ substrings.rb
@@ -1,14 +1,25 @@
-# Compare the final array to the dictionary once again for a true match. 
+# Compare the final array to the dictonary once again for a true match. 
 # Only then when you scan or any? for the array. This is opposed to having 
 # a bunch of small collections of letters that make no sense like ["o","w"]
 
+# This version searches the dictionary for the string
+
 dictonary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-string = "below down go fuck"
+string = "below"
 
 mug = string.split(' ')
-#"The rain in Spain lies mainly in the plain".scan(/\w+ain/)
-mug.each do |stoop| 
-  p stoop.scan(/#{dictonary}/)
+
+dictonary.map do |stoop| 
+   groot = stoop.to_s.scan(/#{mug}/) # if stoop.include?(dictonary)
+   groot.each do |hiyaw| 
+    p hiyaw if dictonary.include?(hiyaw.to_s)
+   end
 end
 
+
+# This is likey de wey
+# Here we will try assigning the stoop to a variable before passing
+# stoop into its own block 
+
+# Getting close, just need to figure out how to pass this as a block
 

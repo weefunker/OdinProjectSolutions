16697ec4ec80ea817e2014993197d7b3c7e098bf
Alex Overend <weefunker@yahoo.co.uk>
2020-07-24 17:55:44 +0100

    Working solution minus the hash counting
    Now just to cross-reference this script with the one on my dev machine and find out how to spit out the result as a hash with counts.

--- substrings.rb
+++ substrings.rb
@@ -1,25 +1,20 @@
-# Compare the final array to the dictonary once again for a true match. 
-# Only then when you scan or any? for the array. This is opposed to having 
-# a bunch of small collections of letters that make no sense like ["o","w"]
-
-# This version searches the dictionary for the string
 
 dictonary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 string = "below"
 
-mug = string.split(' ')
+mug = string.split('')
+
+groot = []
 
 dictonary.map do |stoop| 
-   groot = stoop.to_s.scan(/#{mug}/) # if stoop.include?(dictonary)
-   groot.each do |hiyaw| 
-    p hiyaw if dictonary.include?(hiyaw.to_s)
-   end
+   groot << stoop.to_s.scan(/#{mug}/).join('') 
+   groot.flatten!
 end
 
+groot.each do |hiyaw| 
+    if dictonary.include?(hiyaw) 
+        p hiyaw 
+    end
+end
 
-# This is likey de wey
-# Here we will try assigning the stoop to a variable before passing
-# stoop into its own block 
-
-# Getting close, just need to figure out how to pass this as a block
-
+# This soultion just needs to count the results in a hash and the answer is complete
